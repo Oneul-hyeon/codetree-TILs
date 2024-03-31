1e469f1521c84e10f97dfa5e22a4e1650c46f2b1
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2024-03-31 19:53:10 +0900

    Title: 포탑 부수기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240331/포탑 부수기/destroy-the-turret.py
+++ 240331/포탑 부수기/destroy-the-turret.py
@@ -77,70 +77,98 @@ def select_target(attacker) :
     if len(min_sum) == 1 : return min_sum[0]
     # 2-7. 열 값이 가장 작은 포탑 공격 대상자 선정
     return sorted(min_sum, key = lambda x : x[1])[0]
-# 3. 레이저 공격 범위 함수
+# 3. 레이저 공격 함수
 def laser_attack_range(attacker, target) :
+    global graph, turret
     x, y = attacker
+    state = False
     # 3-1. 큐 생성 후 현재 위치 삽입
     queue = deque()
-    queue.append([(x, y)])
+    queue.append((x, y))
+    # 3-2. 경로 추적 리스트 생성
+    route = [[False for _ in range(m)] for _ in range(m)]
     # 3-2.
     while queue :
-        # 3-2-1. 경로 반환
-        route = queue.popleft()
-        x, y = route[-1]
+        # 3-2-1. 위치 인덱스 반환
+        x, y = queue.popleft()
         # 3-2-2.
         for dir_x, dir_y in [(0, 1), (1, 0), (0, -1), (-1, 0)] :
             # 다음 위치 설정
             nx, ny = (x + dir_x) % n, (y + dir_y) % m
-            # 목표 타겟에 도달한 경우 경로 반환
-            if (nx, ny) == target : return route[1:]
             # 다음 위치가 부서지지 않은 포탑이면서 방문하지 않은 경우
-            if graph[nx][ny] and (nx, ny) not in route :
-                # 큐에 경로 삽입
-                queue.append((route + [(nx, ny)]))
-    # 3-3. None 반환
-    return None
+            if graph[nx][ny] and not route[nx][ny] :
+                # 경로 입력
+                route[nx][ny] = (x, y)
+                # 목표 타겟에 도달한 경우
+                if (nx, ny) == target :
+                    state = True
+                    break
+                # 큐에 다음 위치 삽입
+                queue.append((nx, ny))
+        # 3-2-3. 레이저 공격이 가능한 경우 while문 탈출
+        if state : True
+    # 3-3. 레이저 공격이 가능한 경우 포탑 공격
+    if state :
+        # 3-3-1. 공격 연관 포탑 리스트 생성
+        attack_related_turret = [attacker]
+        nx, ny = target
+        # 3-3-2. 공격력 변수 생성
+        power = turret[attacker]["power"]
+        # 3-3-3. 
+        while (nx, ny) != attacker :
+            # 공격 연관 포탑 리스트 업데이트
+            attack_related_turret.append((nx, ny))
+            # 타겟 혹은 타겟 주변 포탑에 따른 공격력 설정
+            if (nx, ny) == target : now_power = power
+            else : now_power = power // 2
+            # 포탑 공격
+            graph[nx][ny] = graph[nx][ny] - now_power if graph[nx][ny] > now_power else 0
+            if graph[nx][ny] : turret[(nx, ny)]["power"] -= now_power
+            else : del turret[(nx, ny)]
+            nx, ny = route[nx][ny]
+        # 3-3-4. True 반환
+        return attack_related_turret
+    # 3-4. False 반환
+    return False
 # 4. 포탄 공격 범위 함수
 def shell_attack_range(attacker, target) :
+    global graph, turret
     x, y = target[0], target[1]
-    # 4-1. 포탄 공격 범위 반환
-    attack_range = []
-    for dir_x, dir_y in [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)] :
+    # 4-1. 공격 연관 포탑 리스트 생성
+    attack_related_turret = [attacker]
+    # 4-2. 
+    for dir_x, dir_y in [(0, 0), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)] :
         nx, ny = (x + dir_x) % n, (y + dir_y) % m
+        # 4-2-1. 공격자가 아니면서 부서지지 않은 포탑인 경우
         if (nx, ny) != attacker and graph[nx][ny] :
-            attack_range.append((nx, ny)) 
-    return attack_range
+            # 공격 연관 포탑 리스트 업데이트
+            attack_related_turret.append((nx, ny))
+            # 공격력 변수 생성
+            power = turret[attacker]["power"]
+            # 타겟 혹은 타겟 주변 포탑에 따른 공격력 설정
+            if (nx, ny) == target : now_power = power
+            else : now_power = power // 2
+            # 포탑 공격
+            graph[nx][ny] = graph[nx][ny] - now_power if graph[nx][ny] > now_power else 0
+            if graph[nx][ny] : turret[(nx, ny)]["power"] -= now_power
+            else : del turret[(nx, ny)]
+    # 4-3.
+    return attack_related_turret
 # 5. 공격 함수
 def attack(attacker, target) :
-    global graph, turret
-    # 5-1. 공격력 변수 생성
-    power = turret[attacker]["power"]
-    # 5-2. 공격 범위 지정(타겟 제외)
-    attack_range = laser_attack_range(attacker, target)
-    if attack_range == None : attack_range = shell_attack_range(attacker, target)
-    # 5-3.
-    for x, y in attack_range :
-        # 5-3-1. 공격
-        graph[x][y] = graph[x][y] - power // 2 if graph[x][y] > power // 2 else 0
-        # 5-3-2. 활성화 포탑 정보 업데이트
-        if graph[x][y] : turret[(x, y)]["power"] -= power // 2
-        else : del turret[(x, y)]
-    # 5-4. 타겟 공격
-    tx, ty = target
-    graph[tx][ty] = graph[tx][ty] - power if graph[tx][ty] > power else 0
-    # 5-5. 활성화 포탑 정보 업데이트
-    if graph[tx][ty] : turret[(tx, ty)]["power"] -= power
-    else : del turret[(tx, ty)]
-    # 5-4. 공격 범위 반환
-    return attack_range
+    # 5-1. 레이저 공격
+    attack_related_turret = laser_attack_range(attacker, target)
+    # 5-2. 레이저 공격이 불가능할 경우 포탄 공격
+    if not attack_related_turret : attack_related_turret = shell_attack_range(attacker, target)
+    # 5-3. 공격 연관 포탑 리스트 반환
+    return attack_related_turret
 # 6. 포탑 정비 함수 정의
-def turret_maintenance(attacker, target, attack_range) :
+def turret_maintenance(attacker, target, attack_related_turret) :
     global graph, turret
-    except_index = [attacker] + [target] + attack_range
     # 6-1. 
     for idx, info in turret.items() :
         # 6-1-1. 공격과 관련있는 포탑의 경우 continue
-        if idx in except_index : continue
+        if idx in attack_related_turret : continue
         # 6-1-2. 포탑 정비
         x, y = idx
         graph[x][y] += 1
@@ -169,10 +197,10 @@ if __name__ == "__main__" :
         # 8-4. 공격자 공격 시간 업데이트
         turret[attacker]["time"] = t
         # 8-5. 공격
-        attack_range = attack(attacker, target)
+        attack_related_turret = attack(attacker, target)
         # 8-6. 포탑 정비
-        turret_maintenance(attacker, target, attack_range)
-        # 8-6. 포탑이 1개 남은 경우 for문 탈출
+        turret_maintenance(attacker, target, attack_related_turret)
+        # 8-7. 포탑이 1개 남은 경우 for문 탈출
         if len(turret.keys()) == 1 : break
     # 9. 결과 출력
     ans = -int(1e9)
